4a4025393423310f3752e7cae03c9b942e2b850d
narendra646 <narendrakumar27082006@gmail.com>
2025-03-11 13:24:10 +0530

    Time: 3 ms (90.05%) | Memory: 11.1 MB (57.30%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n=s.size(),ans=0;  
-        int left=0,right=0;
-        vector<int>v(3, 0);
-        while(right<n)
-        {
-            v[s[right]-'a']++;
+        // first method
 
-            while(v[0]>0 && v[1]>0 && v[2]>0 )
-            {
-                ans+=n-right;
+    //     int n=s.size(),ans=0;  
+    //     int left=0,right=0;
+    //     vector<int>v(3, 0);
+    //     while(right<n)
+    //     {
+    //         v[s[right]-'a']++;
 
-                v[s[left]-'a']--;
-                left++;
-            }
-            right++;
-        }
+    //         while(v[0]>0 && v[1]>0 && v[2]>0 )
+    //         {
+    //             ans+=n-right;
 
-       return ans;
+    //             v[s[left]-'a']--;
+    //             left++;
+    //         }
+    //         right++;
+    //     }
+    //    return ans;
+
+
+          // *********** second method *******
+
+    int n=s.size();
+    vector<int>v={-1,-1,-1};
+    int ans=0,k=0;
+    for(int i=0;i<n;i++)
+    {
+        v[s[i]-'a']=k;
+        k++;
+
+
+        ans+=1+min({v[0],v[1],v[2]});
+
+    }
+
+    return ans;
+
+
 
     }
 
