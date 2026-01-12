73537fe60f20b40e03d603295447841e57437a6f
narendra646 <narendrakumar27082006@gmail.com>
2026-01-12 20:26:30 +0530

    Time: 2 ms (35.17%) | Memory: 16.3 MB (57.33%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -20,9 +20,29 @@ public:
         int n=grid.size();
         int m=grid[0].size();
 
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
 
-        return fun(n-1,m-1,grid,dp);
+        // return fun(n-1,m-1,grid,dp);
+
+        // *********** tabulation
+        
+        vector<vector<int>>dp(n,vector<int>(m));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(i==0 && j==0)
+                dp[i][j]=grid[0][0];
+                else
+                {
+                    int up=INT_MAX,left=INT_MAX;
+                    if(i>0) up=grid[i][j]+dp[i-1][j];
+                    if(j>0) left=grid[i][j]+dp[i][j-1];
+                    dp[i][j]=min(up,left);
+                }
+            }
+        }
+        return dp[n-1][m-1];
         
     }
 };
