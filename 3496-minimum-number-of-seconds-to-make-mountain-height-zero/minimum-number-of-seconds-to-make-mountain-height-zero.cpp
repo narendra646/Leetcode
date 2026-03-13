a5f65d94685263909a9f059548155a1f7cc664dd
narendra646 <narendrakumar27082006@gmail.com>
2026-03-13 19:02:21 +0530

    Time: 29 ms (54.84%) | Memory: 28.8 MB (100.00%) - LeetSync

---
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+       long long H=*max_element(workerTimes.begin(), workerTimes.end());
+       long long start=1,end=static_cast<long long>(H)*mountainHeight*(mountainHeight+1)/2;
+       long long ans=0;
+       while(start<=end)
+       {
+            long long mid=start+(end-start)/2;
+            long long count=0;
+            for(int i=0;i<workerTimes.size();i++)
+            {
+                long long work=mid/workerTimes[i];
+                // let this worker reduces the height by k
+                long long k=(-1+sqrt(1+8*work))/2;
+                count+=k;
+            }
+            if(count>=mountainHeight)
+            {
+                ans=mid;
+                end=mid-1;
+            }
+            else
+            start=mid+1;
+       }
+        return ans;
+    }
+};
