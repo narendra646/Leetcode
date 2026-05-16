ea116362ba333201520b1530b751ef3bbe68c2a4
narendra646 <narendrakumar27082006@gmail.com>
2026-05-16 21:02:08 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (5.92%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,30 +1,60 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int ans=-1;
-        int n=arr.size();
-        int end=arr[n-1]+k;
-        for(int i=1;i<=end;i++)
+        // int ans=-1;
+        // int n=arr.size();
+        // int end=arr[n-1]+k;
+        // for(int i=1;i<=end;i++)
+        // {
+        //     bool b=false;
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         if(arr[j]==i)
+        //         {
+        //             b=true;
+        //             break;
+        //         }
+        //     }
+        //     if(b==false)
+        //     {
+        //         k--;
+        //     }
+        //     if(k==0)
+        //     {
+        //         ans=i;
+        //         break;
+        //     }
+        // }
+        // return ans;
+
+                 int n=arr.size();
+        vector<int>v(n);
+        v[0]=arr[0]-1;
+        for(int i=1;i<n;i++)
         {
-            bool b=false;
-            for(int j=0;j<n;j++)
-            {
-                if(arr[j]==i)
-                {
-                    b=true;
-                    break;
-                }
-            }
-            if(b==false)
-            {
-                k--;
-            }
-            if(k==0)
-            {
-                ans=i;
-                break;
-            }
+            v[i]=v[i-1]+(arr[i]-arr[i-1]-1);
         }
-        return ans;
+        int ind=-1,val=0;
+        int start=0,end=n-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(v[mid]>=k)
+            {
+                ind=mid;
+                val=v[mid];
+                end=mid-1;
+            }
+            else
+            start=mid+1;
+        }
+        
+        if(ind==-1)
+        return arr[n-1]+k-v[n-1];
+        
+        int missing=arr[ind]-(val-k+1);
+        
+        return missing;
+
     }
 };
