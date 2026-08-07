eff885d0a7b7434a3ce248631f2449298e150673
narendra646 <narendrakumar27082006@gmail.com>
2026-08-08 00:01:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (89.71%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,19 +11,39 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode*root,vector<int>&ans)
-    {
-        if(!root)
-        return;
-
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
+        // morris order traversal
         vector<int>ans;
-        inorder(root,ans);
+        if(!root)
+        return ans;
+        while(root)
+        { 
+            if(root->left)               // left exist means make the extra edge
+            {
+                // chekc if the left is already traversed
+                TreeNode *curr=root->left;
+                while(curr->right && curr->right!=root)
+                curr=curr->right;
 
-        return ans; 
+                if(curr->right!=NULL)                       // means already traversed 
+                {
+                    curr->right=NULL;
+                    ans.push_back(root->val);
+                    root=root->right;
+                }
+                else
+                {
+                    curr->right=root;
+                    root=root->left;
+                }
+            }
+            else                // does't exist means print the value and move the right
+            {
+                ans.push_back(root->val);
+                root=root->right;
+            }
+        }
+
+        return ans;
     }
 };
